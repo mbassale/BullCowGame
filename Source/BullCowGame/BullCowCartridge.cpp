bbc0f8772e185469e35f716a171d60a1165fcea6
Marco Bassaletti <bassaletti@gmail.com>
2020-07-17 20:08:40 -0400

    Added initial welcome text to terminal.

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,14 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    ClearScreen();
+    PrintLine(TEXT("Marco Bassaletti's Bull Cows Game. Welcome!"));
+    PrintLine(TEXT("Press tab to activate terminal and type commands."));
+    PrintLine(TEXT("Type \"help\" command for help."));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-
+    ClearScreen();
+    FString HiddenWord = TEXT("cake");
 }
